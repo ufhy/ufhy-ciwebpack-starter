42f90d203d0a50f1f4a9c6c6c6ddf462b449c32b
ufhy <metallica.one@live.com>
2019-01-17 22:08:26 +0800

    add pages

--- application/themes/default-theme/assets/js/router/paths.js
+++ application/themes/default-theme/assets/js/router/paths.js
@@ -1,10 +1,12 @@
 import Dashboard from '../pages/dashboard/router';
 import Settings from '../pages/settings/router';
+import Profile from '../pages/profile/router';
+import Users from '../pages/users/router';
 
 export default [
   {
     path: '/',
     redirect: '/dashboard',
   },
-  Dashboard, Settings
+  Dashboard, Profile, Settings, Users
 ];
