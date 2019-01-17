e391399d40d2a84dbc52d1a420aba1dc790e8a4a
ufhy <metallica.one@live.com>
2019-01-17 22:09:06 +0800

    add component loader

---
+++ application/themes/default-theme/assets/js/components/AppLoader.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition name="fade">
+    <div class="app-loader" v-if="value"><span>L O A D I N G</span></div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'app-loader',
+    props: {
+      value: {
+        type: Boolean,
+        default: false,
+      }
+    }
+  }
+</script>
